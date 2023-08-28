7aad0faba5d1ee6a8586e2a002b6c9f05ed64507
MichalisTatas <tatas.michalis@gmail.com>
2023-08-28 22:58:04 +0300

    added idt structs

--- src/idt.h
+++ src/idt.h
@@ -2,5 +2,19 @@
 #define IDT_H
 
 
+struct idt_desc
+{
+    uint16_t offset_1;  // offset bits 0-15
+    uint16_t selector;  // selector thats in our GDT
+    uint8_t zero;       // Does nothing, unused set to zero
+    uint8_t type_attr;  // Descriptor type and attributes
+    uint16_t offset_2;  // offset bits 16-31
+} __attribute__((packed));
+
+struct idtr_desc
+{
+    uint16_t limit // size of descriptor table -1
+    uint32_t base; // base address of the star of the interrupt descriptor table
+} __attribute__((packed));
 
 #endif
