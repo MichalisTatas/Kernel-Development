c0c3c0a675612f830bd696eb1f8d7099b6a1fb88
MichalisTatas <tatas.michalis@gmail.com>
2023-09-30 02:09:54 +0300

    started creating the heap

--- src/config.h
+++ src/config.h
@@ -6,4 +6,10 @@
 
 #define OS_TOTAL_INTERRUPTS 512
 
+// 100 MB instead of figuring out how much ram the user has and adjusting the number 
+#define OS_HEAP_SIZE_BYTES 104857600
+#define OS_HEAP_BLOCK_SIZE 4096
+#define OS_HEAP_ADDRESS    0x01000000
+#define OS_HEAP_TABLE_ADDRESS 0x00007E00
+
 #endif
